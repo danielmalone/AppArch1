e5088449f5fda1b62f0cf5623990788be474643b
Daniel Malone <danieljmalone@gmail.com>
2017-05-19 17:16:21 -0500

    make database read, write work

--- app/src/main/java/com/finepointstudios/apparch1/MainActivity.java
+++ app/src/main/java/com/finepointstudios/apparch1/MainActivity.java
@@ -1,13 +1,63 @@
 package com.finepointstudios.apparch1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.LifecycleActivity;
+import android.arch.persistence.room.Room;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends LifecycleActivity {
+
+    private static final String TAG = "MainActivity";
+
+    Context mContext;
+    AppDatabase mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        mDatabase = Room.databaseBuilder(mContext, AppDatabase.class, "mDatabase-users").build();
+
+        new SaveData().execute();
+
+        new GetData().execute();
+    }
+
+    private class SaveData extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            User user = new User();
+            user.setFirstName("Jake");
+            user.setLastName("Blake");
+//            user.setUid(50);
+//            mDatabase.userDao().insertAll(user);
+
+            return null;
+        }
+    }
+
+    private class GetData extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            List<User> users = mDatabase.userDao().getAll();
+
+            for (int i = 0; i < users.size(); i++) {
+                User thisUser = users.get(i);
+                Log.d(TAG, "doInBackground: User: " + thisUser.getFirstName() + " " + thisUser.getUid() + " " + thisUser.getLastName());
+            }
+
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/finepointstudios/apparch1/User.java
@@ -0,0 +1,45 @@
+package com.finepointstudios.apparch1;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Created by danielmalone on 5/19/17.
+ */
+
+@Entity
+public class User {
+    @PrimaryKey
+    private int uid;
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    @ColumnInfo(name = "last_name")
+    private String lastName;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
